584f6de1de3d02044e7f5687584881b964a1f9ad
zepez <zepezaueralex@gmail.com>
2023-06-28 00:05:57 -0400

    :sparkles: basic seed structure

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,24 +1,95 @@
 import { faker } from "@faker-js/faker";
+import { db, user, profile, project, chapter, type Chapter } from "./index";
+
+const SEED_USER_COUNT = 5;
+const SEED_CHAPTER_COUNT = 4;
 
 const genUser = () => {
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
 
+  const verified = faker.datatype.boolean();
+
   return {
     id: faker.string.uuid(),
     name: firstName + " " + lastName,
-    email: faker.internet.email(firstName, lastName),
-    emailVerified: faker.datatype.boolean(),
+    email: faker.internet.email({ firstName, lastName }),
+    emailVerified: verified ? faker.date.past() : null,
   };
 };
 
-const genProfile = () => ({
-  id: faker.string.uuid(),
-});
+const genProfile = (user: ReturnType<typeof genUser>) => {
+  const firstName = user.name.split(" ")[0];
+  const lastName = user.name.split(" ")[1];
 
-console.log("Seeding users...");
+  return {
+    id: faker.string.uuid(),
+    userId: user.id,
+    username: faker.internet.userName({ firstName, lastName }),
+    bio: faker.lorem.sentence(),
+    image: faker.image.avatar(),
+  };
+};
 
-const user = genUser();
-const profile = genProfile();
+const genProject = () => {
+  const name = faker.lorem.words(3);
 
-console.log({ user, profile });
+  return {
+    id: faker.string.uuid(),
+    name,
+    slug: faker.helpers.slugify(name),
+    description: faker.lorem.sentence(),
+  };
+};
+
+const genChapter = (project: ReturnType<typeof genProject>, order: number) => {
+  const paragrpahs: string[] = [];
+  for (let i = 0; i < 3; i++) {
+    paragrpahs.push(`<p>${faker.lorem.paragraph()}</p>`);
+  }
+
+  return {
+    id: faker.string.uuid(),
+    projectId: project.id,
+    name: faker.lorem.words(5),
+    contentType: "html",
+    content: paragrpahs.join("\n"),
+    order,
+  };
+};
+
+const main = async () => {
+  console.log("Seeding data...");
+
+  const generatedUsers: ReturnType<typeof genUser>[] = [];
+  const generatedProfiles: ReturnType<typeof genProfile>[] = [];
+  const generatedProjects: ReturnType<typeof genProject>[] = [];
+  const generatedChapters: ReturnType<typeof genChapter>[] = [];
+
+  for (let i = 0; i < SEED_USER_COUNT; i++) {
+    const generatedUser = genUser();
+    generatedUsers.push(generatedUser);
+
+    const generatedProfile = genProfile(generatedUser);
+    generatedProfiles.push(generatedProfile);
+
+    const generatedProject = genProject();
+    generatedProjects.push(generatedProject);
+
+    for (let j = 0; j < SEED_CHAPTER_COUNT; j++) {
+      const generatedChapter = genChapter(generatedProject, j + 1);
+      generatedChapters.push(generatedChapter);
+    }
+  }
+
+  await db.transaction(async (tx) => {
+    await tx.insert(user).values(generatedUsers);
+    await tx.insert(profile).values(generatedProfiles);
+    await tx.insert(project).values(generatedProjects);
+
+    // TODO: typescript is claiming projectId doesn't exist on the type. Not sure why.
+    await tx.insert(chapter).values(generatedChapters as unknown as Chapter[]);
+  });
+};
+
+void main();
