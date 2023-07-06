1d1012bef0d372556cd30ca31fdcd2713f3626e3
zepez <zepezaueralex@gmail.com>
2023-07-06 14:55:07 -0400

    :bug: remove unused import

--- packages/web/src/app/p/[slug]/chapters/[order]/page.tsx
+++ packages/web/src/app/p/[slug]/chapters/[order]/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import parse from "html-react-parser";
 import { formatDistanceToNow } from "date-fns";
