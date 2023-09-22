8a640de2c1099cc4750157a5c5dbeace1d3059a9
zepez <zepezaueralex@gmail.com>
2023-09-22 05:54:15 -0400

    :construction: bump cover resolution and allow undefined style

--- packages/ai/src/medium/image.ts
+++ packages/ai/src/medium/image.ts
@@ -7,25 +7,25 @@ import {
 type PromptImageOptions = { text: string; weight: number }[];
 
 export const aiImage = async (
-  style: StabilityImageGenerationStylePreset,
+  style: StabilityImageGenerationStylePreset | null,
   prompt: PromptImageOptions
 ) => {
-  return await generateImage(
-    new StabilityImageGenerationModel({
-      model: "stable-diffusion-xl-beta-v2-2-2",
-      cfgScale: 7,
-      stylePreset: style,
-      height: 896,
-      width: 512,
-      samples: 1,
-      steps: 50,
-    }),
-    [
-      ...prompt,
-      {
-        text: "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face",
-        weight: -1,
-      },
-    ]
-  );
+  const opts = {
+    model: "stable-diffusion-xl-1024-v1-0",
+    cfgScale: 7,
+    height: 1216,
+    width: 832,
+    samples: 1,
+    steps: 50,
+  };
+
+  if (style) Object.assign(opts, { stylePreset: style });
+
+  return await generateImage(new StabilityImageGenerationModel(opts), [
+    ...prompt,
+    {
+      text: "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off, low contrast, underexposed, overexposed, bad art, beginner, amateur, distorted face",
+      weight: -1,
+    },
+  ]);
 };
