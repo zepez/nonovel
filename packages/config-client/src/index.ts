deb41678634ac449d8f4a876ef95134177e7a4da
zepez <zepezaueralex@gmail.com>
2023-10-04 23:37:38 -0400

    :sparkles: migrate to plausible over google analytics

--- packages/config-client/src/index.ts
+++ packages/config-client/src/index.ts
@@ -2,10 +2,8 @@ import * as z from "zod";
 
 const schema = z.object({
   NEXT_PUBLIC_S3_DOMAIN: z.string(),
-  NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string(),
 });
 
 export default schema.parse({
   NEXT_PUBLIC_S3_DOMAIN: process.env.NEXT_PUBLIC_S3_DOMAIN,
-  NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
 });
--- packages/web/src/components/shared/analytics.tsx
+++ packages/web/src/components/shared/analytics.tsx
@@ -1,21 +1,11 @@
 import Script from "next/script";
-import config from "@nonovel/config-client";
 
 export const Analytics = () => {
   return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${config.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-      />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', '${config.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
-      </Script>
-    </>
+    <Script
+      defer
+      data-domain="nonovel.io"
+      src="https://plausible.io/js/script.js"
+    />
   );
 };
